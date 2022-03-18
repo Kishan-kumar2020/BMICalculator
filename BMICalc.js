01ad8169e79055d7b51cca1654bee70a5e460843
Kishan-kumar2020 <67338623+Kishan-kumar2020@users.noreply.github.com>
2022-03-18 07:49:35 +0530

    Add files via upload

---
+++ BMICalc.js
@@ -0,0 +1,25 @@
+function bMICalculator() {
+    let num = parseInt(document.getElementById("id1").value)*30.48;
+    let num1 = parseInt(document.getElementById("id2").value)*2.54;
+    let num2 = parseInt(document.getElementById("id3").value);
+    let a;
+    num = num + num1;
+    a = (num2/num/num)*10000;
+    document.getElementById("result").innerHTML = a.toPrecision(3);
+    if(a=="" || isNaN(a)) {
+        document.getElementById("result").innerHTML = "Result";
+        alert("Enter the required field");
+    }
+    if(a<18.5) {
+        document.getElementById("note").innerHTML = "Underweight";
+    }
+    else if(a>=18.5 && a<=24.9) {
+        document.getElementById("note").innerHTML = "Normalweight";
+    }
+    else if(a>=25 && a<=29.9) {
+        document.getElementById("note").innerHTML = "Overweight";
+    }
+    else if(a>=30) {
+        document.getElementById("note").innerHTML = "Obesity";
+    }
+}
